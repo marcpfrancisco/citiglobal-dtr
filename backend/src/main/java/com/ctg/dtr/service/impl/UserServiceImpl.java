97c9ae1a42e3f77c32903c34b27495028ed98e27
Bryan Caliwag <iambryancaliwag@gmail.com>
2023-03-04 15:38:48 +0800

    Remove unused packaged

--- backend/src/main/java/com/ctg/dtr/service/impl/UserServiceImpl.java
+++ backend/src/main/java/com/ctg/dtr/service/impl/UserServiceImpl.java
@@ -1,10 +1,8 @@
 package com.ctg.dtr.service.impl;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
