776fe3db6cb9894fba9fd815821eff2215837494
Bryan Caliwag <iambryancaliwag@gmail.com>
2023-03-05 18:37:45 +0800

    Add JWT on Swagger Configuration

--- backend/src/main/java/com/ctg/dtr/BackendApplication.java
+++ backend/src/main/java/com/ctg/dtr/BackendApplication.java
@@ -2,12 +2,7 @@ package com.ctg.dtr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 
-@OpenAPIDefinition(info =
-    @Info(title = "Citi Global API", version = "3.0", description = "Documentation Citi Global API v3.0")
-)
 @SpringBootApplication
 public class BackendApplication {
 
---
+++ backend/src/main/java/com/ctg/dtr/config/OpenAPIConfiguration.java
@@ -0,0 +1,34 @@
+package com.ctg.dtr.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info =
+    @Info(title = "Citi Global API", version = "3.0", description = "Documentation Citi Global API v3.0")
+)
+public class OpenAPIConfiguration {
+
+    @Bean
+    public OpenAPI customizeOpenAPI() {
+
+        final String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement()
+                .addList(securitySchemeName))
+                .components(new Components()
+                .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                .name(securitySchemeName)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .description("Provide the JWT token. JWT token can be obtained from the <strong>Authentication Controller</strong>.")
+                .bearerFormat("JWT")));
+    }
+}
