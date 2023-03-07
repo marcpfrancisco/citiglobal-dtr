38ced0edb0b8da34d0dd8a524c03a3ce09de3c44
Bryan Caliwag <iambryancaliwag@gmail.com>
2023-03-07 19:37:25 +0800

    [TIMESHEET] Update Authentication

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -14,3 +14,4 @@ INSERT INTO subject (id, created_at, updated_at, published_at, is_active, subjec
 
 -- USER
 INSERT INTO user (id, created_at, updated_at, published_at, is_active, first_name, middle_name, last_name, mobile_no, student_no, rfid_no, username, password, section_id, role_id) VALUES (1, NOW(), NOW(), NOW(), 1, 'JOHN', 'SEEDER', 'DOE', '09123456789', '12345-6789', '1142445230', 'admin', '$2a$10$q.sDwfaG6XRq4QLEN7MjRei/iF8M23WA5vFEEiOr7nxP6jmFT0IBm', NULL, 1);
+INSERT INTO user (id, created_at, updated_at, published_at, is_active, first_name, middle_name, last_name, mobile_no, student_no, rfid_no, username, password, section_id, role_id) VALUES (2, NOW(), NOW(), NOW(), 1, 'KRAM', 'SEEDER', 'GLOW', '09123456789', '12345', '1234567890', 'user', '$2a$10$FZJnw5zDKNp8CMqGrv8YeelQYMmNJt8ojQ8dVbwn44woOo382auOa', 1, 3);
