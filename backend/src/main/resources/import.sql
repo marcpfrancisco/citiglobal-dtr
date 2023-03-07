3a3b61d9f2a9da404a9cd0f019259fa1c128964f
Bryan Caliwag <iambryancaliwag@gmail.com>
2023-03-07 14:59:30 +0800

    Add user seeder

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -14,9 +14,4 @@ INSERT INTO subject (id, created_at, updated_at, published_at, is_active, subjec
 INSERT INTO subject (id, created_at, updated_at, published_at, is_active, subject_code, description, day, start_time, end_time, grace_period, units, section_id) VALUES (4, NOW(), NOW(), NOW(), 1, 'ERTX', 'SEEDER', 'WEDNESDAY', '08:00', '10:00', '08:15', 3, 2);
 
 -- -- USER
--- INSERT INTO user (id, created_at, updated_at, published_at, is_active, first_name, middle_name, last_name, mobile_number, student_id, rfid_no, section_id, role_id) VALUES (1, NOW(), NOW(), NOW(), 1, 'JOHN', 'SEEDER', 'DOE', '09123456789', '12345-6789', '1234567', 2, 2);
--- INSERT INTO user (id, created_at, updated_at, published_at, is_active, first_name, middle_name, last_name, mobile_number, student_id, rfid_no, section_id, role_id) VALUES (2, NOW(), NOW(), NOW(), 1, 'MARC', 'PENTATONIX', 'CLAYROE', '09123456789', '123-456', '123456', 2, 3);
-
--- -- USER LOGIN
--- INSERT INTO user_login (id, created_at, updated_at, username, password) VALUES (1, NOW(), NOW(), 'admin', '$2a$10$6MkbFPutD0h0YVPU9SOv5uFYyapfm5aRta3.UtbJY8wHwS2dogw3q');
-
+INSERT INTO user (id, created_at, updated_at, published_at, is_active, first_name, middle_name, last_name, mobile_no, student_no, rfid_no, username, password, section_id, role_id) VALUES (1, NOW(), NOW(), NOW(), 1, 'JOHN', 'SEEDER', 'DOE', '09123456789', '12345-6789', '1142445230', 'admin', '$2a$10$q.sDwfaG6XRq4QLEN7MjRei/iF8M23WA5vFEEiOr7nxP6jmFT0IBm', NULL, 1);
