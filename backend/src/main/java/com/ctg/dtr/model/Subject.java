3e72c856ee0abee4d307c77718915e7cd0c5367a
Bryan Caliwag <iambryancaliwag@gmail.com>
2023-03-18 11:25:28 +0800

    - Fixed All IDs

--- backend/src/main/java/com/ctg/dtr/model/Subject.java
+++ backend/src/main/java/com/ctg/dtr/model/Subject.java
@@ -8,13 +8,15 @@ import org.hibernate.annotations.UpdateTimestamp;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 
 @Data
 @AllArgsConstructor
@@ -23,7 +25,16 @@ import lombok.NoArgsConstructor;
 public class Subject {
 
     @Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(generator = "subject-sequence-generator")
+    @GenericGenerator(
+      name = "subject-sequence-generator",
+      strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+      parameters = {
+        @Parameter(name = "sequence_name", value = "subject_seq"),
+        @Parameter(name = "initial_value", value = "3"),
+        @Parameter(name = "increment_size", value = "1")
+        }
+    )
 	private Long id;
 
     @CreationTimestamp
--- backend/src/main/java/com/ctg/dtr/model/User.java
+++ backend/src/main/java/com/ctg/dtr/model/User.java
@@ -12,7 +12,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -20,6 +19,9 @@ import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 
 @Data
 @AllArgsConstructor
@@ -29,7 +31,16 @@ import lombok.NoArgsConstructor;
 public class User {
 
     @Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(generator = "user-sequence-generator")
+    @GenericGenerator(
+      name = "user-sequence-generator",
+      strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+      parameters = {
+        @Parameter(name = "sequence_name", value = "user_seq"),
+        @Parameter(name = "initial_value", value = "3"),
+        @Parameter(name = "increment_size", value = "1")
+        }
+    )
 	private Long id;
 
     @CreationTimestamp
