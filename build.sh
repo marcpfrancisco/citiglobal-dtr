b91ebda733e471171a21da1029a7ea75ad2c7b46
Marc Francisco <marcpfrancisco@gmail.com>
2023-03-18 16:36:06 +0800

    ADDED build command for jar file

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 target=$1
 
-if [ "$target" == "piams" ]
+if [ "$target" == "frontend" ]
 then
   cp frontend/src/environments/environment.prod.ts frontend/src/environments/environment.ts
 fi
 
 mvn clean install
-rm piams-prod/piams-app.jar
-mv backend/target/piams-app.jar piams-prod
+rm citiglobal-prod/citiglobal-app.jar
+mv backend/target/citiglobal-app.jar citiglobal-prod
 
 cp frontend/src/environments/environment.test.ts frontend/src/environments/environment.ts
  
